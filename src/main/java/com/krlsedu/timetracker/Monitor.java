b100c0046ff6848eefcc57b69f5384728c17e7c9
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2020-05-20 13:07:01 -0300

    corrigido bug de saida de idle

--- src/main/java/com/krlsedu/timetracker/Monitor.java
+++ src/main/java/com/krlsedu/timetracker/Monitor.java
@@ -1,15 +1,6 @@
 package com.krlsedu.timetracker;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Monitor {
-	
-	private static final int MAX_TITLE_LENGTH = 1024;
-	
-	private static final Map<String, Long> timeMap = new HashMap<>();
-	
-	private static final int waitTime = 100;
 	
 	public static void main(String[] args) throws Exception {
 		Core.start();
