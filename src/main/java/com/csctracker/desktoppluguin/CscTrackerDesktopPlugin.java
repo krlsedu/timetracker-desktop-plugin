b64b34a3152e3c382c5f96b0c56f1b0870dc80e4
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-10-13 16:18:49 -0300

    Notification sync

--- src/main/java/com/csctracker/desktoppluguin/CscTrackerDesktopPlugin.java
+++ src/main/java/com/csctracker/desktoppluguin/CscTrackerDesktopPlugin.java
@@ -2,6 +2,8 @@ package com.csctracker.desktoppluguin;
 
 
 import com.csctracker.desktoppluguin.core.NotificationSync;
+import com.csctracker.desktoppluguin.desktop.Core;
+import com.csctracker.desktoppluguin.desktop.Tray;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
